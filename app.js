39c0cd820a46ce916dac7c053c08eac1b604b8f4
it22188236 <144685319+it22188236@users.noreply.github.com>
2025-03-01 09:17:55 +0530

    app js

--- app.js
+++ app.js
@@ -1,14 +1,2 @@
 console.log("Hello World!");
 
-//Read file
-const fs = require("fs");
-fs.readFile("file.txt", "utf8", function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
-
-//Write file
-fs.writeFile("file.txt", "Hello World!", function (err) {
-  if (err) throw err;
-  console.log("File saved!");
-});
