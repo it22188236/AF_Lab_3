e05b65f2fbcd01228f6c6620d46f4a1e98f1de24
it22188236 <144685319+it22188236@users.noreply.github.com>
2025-03-01 09:15:13 +0530

    App file

--- app.js
+++ app.js
@@ -1,23 +1,2 @@
 console.log("Hello World!");
 
-//Read file
-const fs = require("fs");
-fs.readFile("file.txt", "utf8", function (err, data) {
-  if (err) throw err;
-  console.log(data);
-});
-
-//Write file
-fs.writeFile("file.txt", "Hello World!", function (err) {
-  if (err) throw err;
-  console.log("File saved!");
-});
-
-
-//create a server
-const http = require('http');
-http.createServer(function (req, res) {
- res.writeHead(200, {'Content-Type': 'text/html'});
- res.write('Hello World!');
- res.end();
-}).listen(8080);
