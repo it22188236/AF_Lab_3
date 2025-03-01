6f1efa177e5b4a68cf0bc39c97ca70455f9a45e0
it22188236 <144685319+it22188236@users.noreply.github.com>
2025-03-01 09:02:30 +0530

    Update write file option

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ fs.readFile("file.txt", "utf8", function (err, data) {
   if (err) throw err;
   console.log(data);
 });
+
+//Write file
+fs.writeFile("file.txt", "Hello World!", function (err) {
+  if (err) throw err;
+  console.log("File saved!");
+});
